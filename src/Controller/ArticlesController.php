6cd4d1c43dbc55385b3fcd9beed5cdfdaff4bc4f
JosePaniagua <jose.paniagua.gomez@gmail.com>
2020-08-05 11:38:40 +0200

    Adding add method

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -112,5 +113,41 @@ namespace App\Controller;
 // }
 class ArticlesController extends AppController
 {
-    
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact(('articles')));
+    }
+
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            //Check if print works and how the request looks
+            pr($this->request->getData());
+            $article  = $this->Articles->patchEntity($article, $this->request->getData());
+            //hardcoding the user_id is temporaty, and should be removed; 
+            $article->user_id = 1;
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article'));
+        }
+        $this->set('article', $article);
+    }
 }
